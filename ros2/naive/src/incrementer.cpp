59e9371981d2f7009549e8d42e149e38a5f2ae69
Bilal Gill <bilal.gill@picknik.ai>
2022-07-15 09:42:17 -0400

    Updated documentation, comments, and style based on feedback

--- ros2/naive/src/incrementer.cpp
+++ ros2/naive/src/incrementer.cpp
@@ -1,15 +1,19 @@
 #include "naive/incrementer.hpp"
 
 #include <memory>
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int64.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int64.hpp>
 
-Incrementer::Incrementer(std::shared_ptr<rclcpp::Node> node, const std::string& in_topic, const std::string& out_topic)
-: node_{std::move(node)}, 
-  pub_{node_->create_publisher<std_msgs::msg::Int64>(out_topic,QUEUE_SIZE)},
-  sub_{node_->create_subscription<std_msgs::msg::Int64>(in_topic, QUEUE_SIZE, [this](const std_msgs::msg::Int64::UniquePtr msg)
-                                                                                  {
-                                                                                    std_msgs::msg::Int64 incremented;
-                                                                                    incremented.data = msg->data + 1;
-                                                                                    pub_->publish(incremented);
-                                                                                  })} {}
+namespace {
+  constexpr auto queue_size = 10;
+}
+
+Incrementer::Incrementer(std::shared_ptr<rclcpp::Node> node, std::string const& in_topic, std::string const& out_topic)
+: node_{std::move(node)},
+  pub_{node_->create_publisher<std_msgs::msg::Int64>(out_topic,queue_size)},
+  sub_{node_->create_subscription<std_msgs::msg::Int64>(in_topic, queue_size,
+                                                        [this](std_msgs::msg::Int64::UniquePtr const msg) {
+                                                          std_msgs::msg::Int64 incremented;
+                                                          incremented.data = msg->data + 1;
+                                                          pub_->publish(incremented);
+                                                        })} {}
