06f71047163d2ed166bd496b672b514cbcde4082
AndyZe <andyz@utexas.edu>
2022-12-07 12:44:25 -0700

    Add a minimal ROS2 integration test example (#11)
    * Add a minimal ROS2 integration test example
    
    * Address code review
    
    * Add more comments about gtest fixture
    
    Co-authored-by: Griswald Brooks <griswald.brooks@picknik.ai>

---
+++ ros2/minimal_integration_test/README.md
@@ -0,0 +1,13 @@
+## Minimal Integration Test
+
+To build the test:
+
+```
+colcon build
+```
+
+To run the test:
+
+```
+colcon test --packages-select minimal_integration_test
+```
---
+++ ros2/minimal_integration_test/test/basic_test.cpp
@@ -0,0 +1,52 @@
+// Description: Test if a simple task plan works
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>
+#include <memory>
+
+namespace minimal_integration_test {
+class TaskPlanningFixture : public testing::Test {
+ public:
+  // Setup things that should occur before every test instance should go here
+  // https://google.github.io/googletest/faq.html#CtorVsSetUp
+  TaskPlanningFixture()
+      : node_(std::make_shared<rclcpp::Node>("basic_test"))
+  {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH CONSTRUCTOR!!");
+  }
+
+  // Things that can't be done in the ctor should go here, such as calling
+  // ASSERT_XX macros in the fixture vs the TEST_F
+  void SetUp() override {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH SETUP!!");
+  }
+
+  // Place most cleanup actions here, if needed
+  ~TaskPlanningFixture() {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH DESTRUCTOR!!");
+  }
+
+  // Cleanup actions that could throw an exception
+  void TearDown() override {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH TEARDOWN!!");
+  }
+
+ protected:
+  rclcpp::Node::SharedPtr node_;
+};
+
+TEST_F(TaskPlanningFixture, TrueIsTrueTest) {
+  RCLCPP_ERROR_STREAM(node_->get_logger(), "TESTING!!");
+  EXPECT_TRUE(true);
+}
+}  // namespace minimal_integration_test
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  int result = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  RCLCPP_ERROR_STREAM(rclcpp::get_logger("basic_test"), "DONE SHUTTING DOWN ROS!!");
+  return result;
+}
