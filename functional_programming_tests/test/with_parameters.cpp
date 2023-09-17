fd76f6092e9b9ec5ffdabbdb7871b09459ae4a4d
Griswald Brooks <griswald.brooks@picknik.ai>
2023-09-17 11:46:13 -0700

    Update functional_programming_tests/test/with_parameters.cpp
    Co-authored-by: Bilal Gill <bilal.gill@picknik.ai>

--- functional_programming_tests/test/with_parameters.cpp
+++ functional_programming_tests/test/with_parameters.cpp
@@ -462,8 +462,7 @@ TEST_F(TaskPlanningFixture, path_generated) {
   EXPECT_EQ(result.second, rclcpp::FutureReturnCode::SUCCESS)
       << "Generating path failed";
 
-  // THEN the global path produced should have one element, which is the
-  // start/goal position
+  // THEN the global path produced should be the same as the expected set of Positions
   std::vector<Position> const expected{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0},
                                        {5, 0}, {6, 0}, {7, 0}, {7, 1}, {7, 2},
                                        {7, 3}, {7, 4}, {7, 5}, {7, 6}, {7, 7}};
